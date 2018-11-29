018709b7311b7c73812dce5b639c21af3dc28fd6
ede1998 <online@erik-hennig.me>
2018-11-29 23:38:55 +0100

    added FF_3,2

--- forestplot.py
+++ forestplot.py
@@ -1,11 +1,17 @@
 import igraph
 from igraph import *
 
-def make_trees():
+def make_FF31():
     trees = [Graph(n=3, edges=[(0,1),(0,2)], directed=False),
              Graph(n=3, edges=[(1,0),(1,2)], directed=False),
 	     Graph(n=3, edges=[(2,0),(2,1)], directed=False)]
-    return trees
+    return join_graphs(*[t.copy() for t in trees for i in range(3)])
+
+def make_FF32():
+    trees = [Graph(n=3, edges=[(0,2)], directed=False),
+             Graph(n=3, edges=[(0,1)], directed=False),
+	     Graph(n=3, edges=[(1,2)], directed=False)]
+    return join_graphs(*[t.copy() for t in trees for i in range(2)])
 
 def join_graphs(*graphs):
     ret = Graph()
@@ -18,15 +24,24 @@ def join_graphs(*graphs):
 
 
 if __name__ == "__main__":
-    trees = make_trees()
-
-    forest = join_graphs(*[t.copy() for t in trees for i in range(3)])
+    # plot FF_3,1
+    forest = make_FF31()
     print(forest)
-    roots=[4*i-(i//3)*3 for i in range(9)]
 
+    roots=[4*i-(i//3)*3 for i in range(9)]
     visual_style = {}
     visual_style["vertex_label"] = [(i.index % 3)+1 for i in forest.vs]
     visual_style["layout"] = forest.layout_reingold_tilford(root=[3*i+(i//3) for i in range(9)])
     visual_style["vertex_color"] = ["red" if i in roots else "gray" for i in range(27)]
     plot(forest, bbox=(600,150), **visual_style)
 
+    # plot FF_3,2
+    forest = make_FF32()
+    print(forest)
+
+    roots=[0,1,4,5,6,8,10,11,12,13,15,17]
+    visual_style = {}
+    visual_style["vertex_label"] = [(i.index % 3)+1 for i in forest.vs]
+    visual_style["layout"] = forest.layout_reingold_tilford()
+    visual_style["vertex_color"] = ["red" if i in roots else "gray" for i in range(18)]
+    plot(forest, bbox=(600,150), **visual_style)
