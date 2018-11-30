a1801545d80477a07c5fdf7c4cef8d94c8881bc2
ede1998 <online@erik-hennig.me>
2018-11-30 14:06:47 +0100

    all forests are now generated

--- forestplot.py
+++ forestplot.py
@@ -13,6 +13,9 @@ def make_FF32():
 	     Graph(n=3, edges=[(1,2)], directed=False)]
     return join_graphs(*[t.copy() for t in trees for i in range(2)])
 
+def make_F33():
+    return Graph(n=3)
+
 def join_graphs(*graphs):
     ret = Graph()
     offset = 0
@@ -22,26 +25,46 @@ def join_graphs(*graphs):
         offset += g.vcount()
     return ret
 
+def plot_graphs(graph, filename, **vs):
+    bbox=(1000,250)
+    if len(filename) == 0:
+        plot(forest, bbox=bbox, **visual_style)
+    else:
+        plot(forest, filename, bbox=bbox, **visual_style)
+
 
 if __name__ == "__main__":
     # plot FF_3,1
     forest = make_FF31()
     print(forest)
 
-    roots=[4*i-(i//3)*3 for i in range(9)]
     visual_style = {}
+    visual_style["vertex_size"] = 45
+    visual_style["margin"] = 80
+
+    roots = [4*i-(i//3)*3 for i in range(9)]
     visual_style["vertex_label"] = [(i.index % 3)+1 for i in forest.vs]
     visual_style["layout"] = forest.layout_reingold_tilford(root=[3*i+(i//3) for i in range(9)])
     visual_style["vertex_color"] = ["red" if i in roots else "gray" for i in range(27)]
-    plot(forest, bbox=(600,150), **visual_style)
+    plot_graphs(forest, "ff31.png", **visual_style)
 
     # plot FF_3,2
     forest = make_FF32()
     print(forest)
 
-    roots=[0,1,4,5,6,8,10,11,12,13,15,17]
-    visual_style = {}
+    root = [0,1,4,5,6,8,10,11,12,13,15,17]
     visual_style["vertex_label"] = [(i.index % 3)+1 for i in forest.vs]
     visual_style["layout"] = forest.layout_reingold_tilford()
     visual_style["vertex_color"] = ["red" if i in roots else "gray" for i in range(18)]
-    plot(forest, bbox=(600,150), **visual_style)
+    plot_graphs(forest, "ff32.png", **visual_style)
+
+    # plot FF_3,3
+    forest = make_F33()
+    print(forest)
+
+    roots = [i for i in range(3)]
+    visual_style["vertex_label"] = [(i.index % 3)+1 for i in forest.vs]
+    visual_style["layout"] = forest.layout_reingold_tilford()
+    visual_style["vertex_color"] = ["red" if i in roots else "gray" for i in range(3)]
+    plot_graphs(forest, "ff33.png", **visual_style)
+
